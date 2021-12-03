1478bb784ebcb3309eca3e3fa7d2f3542b2b55c5
Phuong Nguyen Viet <nguyenvietphuong271100@gmail.com>
2021-12-03 16:37:15 +0700

    Update BM and Huong dan cai dat

--- QuanLyThuVien/models/sach.py
+++ QuanLyThuVien/models/sach.py
@@ -11,8 +11,11 @@ class Sach(models.Model):
     anh = fields.Binary(string='Ảnh')
     so_trang = fields.Integer(string='Số trang')
     the_loai = fields.Many2many(comodel_name='model.theloai', inverse_name='ten_sach', string='Thể loại')
+    ds_pm = fields.Many2many(comodel_name='model.phieumuon', string='Sách')
+    pm_count = fields.Integer(compute='count_da_muon', string='Đã mượn')
     mo_ta = fields.Text(string='Mô tả')
     anh_sach = fields.Binary(string='Ảnh sách')
+    active = fields.Boolean(default=True)
 
     @api.model
     def create(self, vals):
@@ -27,10 +30,21 @@ class Sach(models.Model):
     def unlink(self):
         return super(Sach, self).unlink()
 
+    def an(self):
+        self.active = False
+
+    def hien(self):
+        self.active = True
+
     @api.constrains('so_trang')
     def validate_sotrang(self):
         if not self.so_trang or self.so_trang <= 0:
             raise ValidationError('Số trang phải lớn hơn 0!')
+
+    @api.depends('ds_pm')
+    def count_da_muon(self):
+        for rec in self:
+            rec.pm_count = len(rec.ds_pm)
 
 
 
